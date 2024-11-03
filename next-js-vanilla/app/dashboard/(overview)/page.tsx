42bb25fa2d064a69a476414219ec334ece64700e
sankalp-it <31386876+sankalp-it@users.noreply.github.com>
2024-11-03 11:19:18 -0600

    Chnages related to streaming

--- next-js-vanilla/app/dashboard/(overview)/page.tsx
+++ next-js-vanilla/app/dashboard/(overview)/page.tsx
@@ -2,14 +2,16 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchRevenue, 
-         fetchLatestInvoices,
-         fetchCardData
-} from '@/app/lib/data';
+import { fetchCardData } from '@/app/lib/data';
+import { Suspense } from 'react';
+import { RevenueChartSkeleton,
+         LatestInvoicesSkeleton, 
+} from '@/app/ui/skeletons';
+ 
 
 export default async function Page() {
 
-const revenue = await fetchRevenue();
+//const revenue = await fetchRevenue();
 const latestInvoices = await fetchLatestInvoices();
 const {
     numberOfInvoices,
@@ -33,8 +35,14 @@ const {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        {<RevenueChart revenue={revenue}  /> }
-        {<LatestInvoices latestInvoices={latestInvoices} /> }
+        {/*<RevenueChart revenue={revenue}  /> */}
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        {/*<LatestInvoices latestInvoices={latestInvoices} /> */}
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
